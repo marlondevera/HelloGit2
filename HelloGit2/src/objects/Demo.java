bf28e76c4b5e2965c796382b02d3d01581185500
marlon.devera <mad4ms@gmail.com>
2014-02-25 11:52:13 -0800

    added println

--- HelloGit2/src/objects/Demo.java
+++ HelloGit2/src/objects/Demo.java
@@ -4,7 +4,12 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			int a;
+			a = 1;
+			System.out.println("HELLO");
+			
+			
+					
 	}
 
 }
